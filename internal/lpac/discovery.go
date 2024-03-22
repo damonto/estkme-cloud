b6e2eeca41a80b22ce09c8388dd612ac21efe618
Damon To <damon.tu@outlook.com>
2024-03-22 22:35:35 +0800

    fix: discovery command not found

--- internal/lpac/discovery.go
+++ internal/lpac/discovery.go
@@ -1,12 +1,12 @@
 package lpac
 
 type DiscoveryResponse struct {
-	RspServer string `json:"rspServer"`
+	RspServerAddress string `json:"rspServerAddress"`
 }
 
-func (c *Cmder) Discovery() (DiscoveryResponse, error) {
-	var response DiscoveryResponse
-	if err := c.Run([]string{"discovery"}, &response, nil); err != nil {
+func (c *Cmder) Discovery() ([]DiscoveryResponse, error) {
+	var response []DiscoveryResponse
+	if err := c.Run([]string{"profile", "discovery"}, &response, nil); err != nil {
 		return response, err
 	}
 	return response, nil
