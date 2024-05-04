dda9249c1d66a212b581ad6f1ed2f93afd55df8b
Damon To <damon.tu@outlook.com>
2024-05-04 12:45:36 +0800

    chore: use 'conn' instead of 'connection' variable name

--- internal/cloud/manager.go
+++ internal/cloud/manager.go
@@ -6,7 +6,7 @@ import (
 )
 
 type Manager interface {
-	Add(id string, connection *Conn)
+	Add(id string, conn *Conn)
 	Remove(id string)
 	Get(id string) (*Conn, error)
 	GetAll() []*Conn
@@ -14,27 +14,27 @@ type Manager interface {
 }
 
 var (
-	ErrConnNotFound = errors.New("connection not found")
+	ErrConnNotFound = errors.New("conn not found")
 )
 
 type manager struct {
-	connections sync.Map
+	conns sync.Map
 }
 
 func NewManager() Manager {
 	return &manager{}
 }
 
-func (m *manager) Add(id string, connection *Conn) {
-	m.connections.Store(id, connection)
+func (m *manager) Add(id string, conn *Conn) {
+	m.conns.Store(id, conn)
 }
 
 func (m *manager) Remove(id string) {
-	m.connections.Delete(id)
+	m.conns.Delete(id)
 }
 
 func (m *manager) Get(id string) (*Conn, error) {
-	conn, ok := m.connections.Load(id)
+	conn, ok := m.conns.Load(id)
 	if !ok {
 		return nil, ErrConnNotFound
 	}
@@ -42,17 +42,17 @@ func (m *manager) Get(id string) (*Conn, error) {
 }
 
 func (m *manager) GetAll() []*Conn {
-	var connections []*Conn
-	m.connections.Range(func(_, value interface{}) bool {
-		connections = append(connections, value.(*Conn))
+	var conns []*Conn
+	m.conns.Range(func(_, value interface{}) bool {
+		conns = append(conns, value.(*Conn))
 		return true
 	})
-	return connections
+	return conns
 }
 
 func (m *manager) Len() int {
 	length := 0
-	m.connections.Range(func(_, _ interface{}) bool {
+	m.conns.Range(func(_, _ interface{}) bool {
 		length++
 		return true
 	})
