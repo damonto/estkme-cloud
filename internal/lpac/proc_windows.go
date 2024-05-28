2c749d171a340acdb62cc86dbc9fc25dd71ce9d2
Damon To <damon.tu@outlook.com>
2024-05-28 11:21:01 +0800

    chore: remove unused LIBCURL environment variable assignment

--- internal/lpac/proc_windows.go
+++ internal/lpac/proc_windows.go
@@ -4,14 +4,10 @@ package lpac
 
 import (
 	"os/exec"
-	"path/filepath"
 	"syscall"
-
-	"github.com/damonto/estkme-cloud/internal/config"
 )
 
 func (c *Cmder) forSystem(cmd *exec.Cmd) {
-	cmd.Env = append(cmd.Env, "LIBCURL="+filepath.Join(config.C.DataDir, "libcurl.dll"))
 	cmd.SysProcAttr = &syscall.SysProcAttr{
 		HideWindow: true,
 	}
