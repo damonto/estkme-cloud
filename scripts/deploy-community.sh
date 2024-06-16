2696eeaf56257985cd439e1b2e47c7e4dd721dbb
Damon To <damon.tu@outlook.com>
2024-06-16 10:51:49 +0800

    chore: disable fail-fast strategy in deploy workflow

---
+++ scripts/deploy-community.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Check if docker is installed
+if ! [ -x "$(command -v docker)" ]; then
+  echo 'Error: docker is not installed.' >&2
+  exit 1
+fi
+
+curl -o Dockerfile https://raw.githubusercontent.com/damonto/estkme-cloud/main/Dockerfile.server
+docker buildx build --file Dockerfile -t estkme-cloud-community:latest .
+docker run -d --restart=always --name estkme-cloud-community -p 1022:22 -p 1888:1888 estkme-cloud-community:latest
